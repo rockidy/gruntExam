efa0c4f57a65128b98eca154f73399f7dc2e40f9
rockidy <cwy@itwork.co.kr>
2017-05-16 17:04:03 +0900

    수정

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,11 @@ module.exports = function(grunt) {
   // 프로젝트 환경설정.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      foo: {
+        src: ['src/app.js']
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> \nauthor: <%=pkg.author%> \n<%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -13,9 +18,11 @@ module.exports = function(grunt) {
     }
   });
 
-  // "uglify" 작업을 위한 플러그인 등록
+  // "jshint" 작업을 위한 플로그인 등록.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // "uglify" 작업을 위한 플러그인 등록.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // 기본 작업에 'uglify' 등록
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
